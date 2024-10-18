7b65a0a6a6ecfebf5973e10aceebfdda4d0b0510
sanchxtene <84470734+sanchxtene@users.noreply.github.com>
2024-10-18 16:51:36 -0300

    botões personagens

---
+++ pipoca-flix/src/components/characters.js
@@ -0,0 +1,10 @@
+
+
+function mostra(e1, e2, e3, e4, e5, e6){
+    document.getElementById(e1).style.display = block;
+    document.getElementById(e2).style.display = none;
+    document.getElementById(e3).style.display = none;
+    document.getElementById(e4).style.display = none;
+    document.getElementById(e5).style.display = none;
+    document.getElementById(e6).style.display = none;
+}
